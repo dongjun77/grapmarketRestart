2db9d602aa31bb11e5858491d37f013256217556
chundong <chundong@chundongui-MacBookAir.local>
2021-08-31 01:53:12 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent />; //MainPage라고 사용해도 된다. <-> MainPageComponent와 같이 읽는다.
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+      </Switch>
+    </div>
+  ); //MainPage라고 사용해도 된다. <-> MainPageComponent와 같이 읽는다.
 }
 
 export default App;
