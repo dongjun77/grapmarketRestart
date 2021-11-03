bb30ba48774e7478a72ce391834459b849719f5a
chundong <chundong@chundongui-MacBookAir.local>
2021-11-03 23:25:01 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production" //cloud환경에서 실행시
+    ? "https://chun-market-server.herokuapp.com"
+    : "http://localhost:8080"; //localhost환경에서 실행시
